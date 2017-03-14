3f122ccfa19327dcd93c7a04a8835fcfdd96524c
Aaron Greenwell <aarongreenwell@Aarons-MacBook-Pro.local>
2017-03-14 17:50:39 -0600

    constructer file added

---
+++ public/controllers/controller.js
@@ -0,0 +1,19 @@
+scotchApp.controller('mainController', function ($scope) {
+    $scope.message = "Something cool to say about the Syllabuster";
+});
+
+scotchApp.controller('dayController', function ($scope) {
+    $scope.message = "Day view below";
+});
+
+scotchApp.controller('weekController', function ($scope) {
+    $scope.message = "Week view below";
+});
+
+scotchApp.controller('loginController', function ($scope) {
+    $scope.message = "Login below";
+});
+
+scotchApp.controller('signupController', function ($scope) {
+    $scope.message = "Signup below";
+});
