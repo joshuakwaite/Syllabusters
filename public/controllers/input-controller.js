142ed66e075b468148b8c0e5efb70c9cb2f799a0
Lance Murdock <gabehare90@gmail.com>
2017-03-15 15:41:36 -0600

    Made the add syllabus button work, and got the syllabus dropdown to show all the user's syllabi.

--- public/controllers/input-controller.js
+++ public/controllers/input-controller.js
@@ -3,7 +3,12 @@ angular.module("scotchApp")
 
         $scope.types = ["Lesson", "Warmup", "Project", "Test"];
 
-        $scope.syllabi = httpService.getSyllabi();
+        $scope.syllabi = httpService.getSyllabi().then(function (response) {
+            console.log(response.data);
+            $scope.name = response.data;
+            return response.data[0].name
+            });
 
+        $scope.addObject = httpService.editSyllabus();
 
     }]);
