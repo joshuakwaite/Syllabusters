58002851f59b4d5c309e0bc1585aa810407f61ee
Lance Murdock <gabehare90@gmail.com>
2017-03-20 15:35:13 -0600

    I added an ng-repeat for the days of the week.

--- public/controllers/week-controller.js
+++ public/controllers/week-controller.js
@@ -2,7 +2,16 @@ var scotchApp = angular.module("scotchApp");
 
 scotchApp.controller('weekController', ["$scope", "httpService", "syllabiService", function ($scope, httpService, syllabiService) {
 
-    console.log(syllabiService.returnSavedCourse())
+    function getWeek(fromDate) {
+        var sunday = new Date(fromDate.setDate(fromDate.getDate() - fromDate.getDay()))
+            , result = [new Date(sunday)];
+        while (sunday.setDate(sunday.getDate() + 1) && sunday.getDay() !== 0) {
+            result.push(new Date(sunday));
+        }
+        return result;
+    }
+
+    $scope.week = getWeek(new Date());
 
 }]);
 
