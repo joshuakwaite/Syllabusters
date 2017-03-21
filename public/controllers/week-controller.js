3b72c2c4e04202728468906e060907ddaec85c77
Lance Murdock <gabehare90@gmail.com>
2017-03-21 16:40:17 -0600

    FINALLY got the days to correspond with the proper assignments.

--- public/controllers/week-controller.js
+++ public/controllers/week-controller.js
@@ -1,6 +1,18 @@
 var scotchApp = angular.module("scotchApp");
 
-scotchApp.controller('weekController', ["$scope", "httpService", "syllabiService", "$location", function ($scope, httpService, syllabiService, $location) {
+scotchApp.controller('weekController', ["$scope", "httpService", "syllabiService", "$location", "$filter", function ($scope, httpService, syllabiService, $location, $filter) {
+
+    function retrieve() {
+        if (syllabiService.returnSavedCourse() === undefined) {
+            alert("test");
+            $location.path("/home");
+        } else {
+            return syllabiService.returnSavedCourse();
+        }
+    }
+
+    retrieve();
+
 
     function getWeek(fromDate) {
         var sunday = new Date(fromDate.setDate(fromDate.getDate() - fromDate.getDay()))
@@ -8,23 +20,30 @@ scotchApp.controller('weekController', ["$scope", "httpService", "syllabiService
         while (sunday.setDate(sunday.getDate() + 1) && sunday.getDay() !== 0) {
             result.push(new Date(sunday));
         }
+
         return result;
     }
 
     $scope.week = getWeek(new Date());
 
 
-    function retrieve() {
-        if (syllabiService.returnSavedCourse() == undefined) {
-            alert("Please select a course");
-            $location.path("/home");
-        } else {
-            var course = syllabiService.returnSavedCourse();
-            $scope.assignments = course.assignments;
-        }
+    var data = syllabiService.returnSavedCourse();
 
+    $scope.work = [];
+
+    function homework() {
+
+        for (var i = 0; i < data.assignments.length; i++) {
+            var dayOfWeek = $filter('date')(data.assignments[i].startDate, "EEEE");
+            data.assignments[i].dayOfWeek = dayOfWeek;
+            $scope.work.push(data.assignments[i]);
+            console.log($scope.work);
+
+
+        }
     }
-    retrieve();
+
+    homework();
 
 
 }]);
