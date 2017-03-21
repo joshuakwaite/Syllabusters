851ad8afbc165f1cccbed072171b755d44a9714d
Josh Waite <kalewaite@gmail.com>
2017-03-21 16:51:48 -0600

    Added watch on week view

--- public/controllers/week-controller.js
+++ public/controllers/week-controller.js
@@ -2,48 +2,47 @@ var scotchApp = angular.module("scotchApp");
 
 scotchApp.controller('weekController', ["$scope", "httpService", "syllabiService", "$location", "$filter", function ($scope, httpService, syllabiService, $location, $filter) {
 
-    function retrieve() {
-        if (syllabiService.returnSavedCourse() === undefined) {
-            alert("test");
-            $location.path("/home");
-        } else {
-            return syllabiService.returnSavedCourse();
-        }
-    }
+    $scope.syllabiService = syllabiService;
 
-    retrieve();
+    $scope.$watch('syllabiService.savedCourse', function (newVal, oldVal) {
 
 
-    function getWeek(fromDate) {
-        var sunday = new Date(fromDate.setDate(fromDate.getDate() - fromDate.getDay()))
-            , result = [new Date(sunday)];
-        while (sunday.setDate(sunday.getDate() + 1) && sunday.getDay() !== 0) {
-            result.push(new Date(sunday));
+        function getWeek(fromDate) {
+            var sunday = new Date(fromDate.setDate(fromDate.getDate() - fromDate.getDay()))
+                , result = [new Date(sunday)];
+            while (sunday.setDate(sunday.getDate() + 1) && sunday.getDay() !== 0) {
+                result.push(new Date(sunday));
+            }
+
+            return result;
         }
 
-        return result;
-    }
-
-    $scope.week = getWeek(new Date());
+        $scope.week = getWeek(new Date());
 
 
-    var data = syllabiService.returnSavedCourse();
+        var data = newVal;
 
-    $scope.work = [];
+        $scope.work = [];
 
-    function homework() {
+        function homework() {
 
-        for (var i = 0; i < data.assignments.length; i++) {
-            var dayOfWeek = $filter('date')(data.assignments[i].startDate, "EEEE");
-            data.assignments[i].dayOfWeek = dayOfWeek;
-            $scope.work.push(data.assignments[i]);
-            console.log($scope.work);
+            for (var i = 0; i < data.assignments.length; i++) {
+                var dayOfWeek = $filter('date')(data.assignments[i].startDate, "EEEE");
+                data.assignments[i].dayOfWeek = dayOfWeek;
+                $scope.work.push(data.assignments[i]);
+                console.log($scope.work);
 
 
+            }
         }
-    }
 
-    homework();
+        homework();
+
+
+
+    }, true);
+
+
 
 
 }]);
